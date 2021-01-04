44e99b66636b88b3ac1b7be4a1f6a1c954eb1a69
kino <kino@GUNPAD.home>
2021-01-03 21:45:50 -0500

    simulation.py

--- simulation.py
+++ simulation.py
@@ -11,9 +11,89 @@ def traditional(prevalence, poolsize):
 	return 0
 
 # matrix pooling
-def matrix(prevalence, matrixsize):
-	retests = 0
-	return retests
+def makematrix(prevalence, matrixsize):
+	samples = matrixsize ** 2
+	m = [0] * samples
+	for a in range(samples):
+		if random.random() < prevalence:
+			m[a] = 1
+	return m
+
+def horizontal(inputmatrix, matrixsize):
+	hoz = []
+	for a in range(matrixsize):
+		hoz.append(inputmatrix[(a * matrixsize):((a+1) * matrixsize)])
+	return hoz
+
+def vertical(inputmatrix, matrixsize):
+	vert = []
+	for a in range(matrixsize):
+		vert.append([])
+		for b in range(matrixsize):
+			vert[a].append(inputmatrix[a + (b * matrixsize)])
+	return vert
+
+def reducepools(inputpools):
+	reduced = [0] * len(inputpools)
+	for a in range(len(inputpools)):
+		for b in range(len(inputpools[a])):
+			if inputpools[a][b] == 1:
+				reduced[a] = 1
+	return reduced
+
+def matrixretests(reducedhoz, reducedvert, matrixsize):
+	r = 0
+	hozpositive = 0
+	vertpositive = 0
+	# assuming a square matrix
+	for a in range(matrixsize):
+		hozpositive += reducedhoz[a]
+		vertpositive += reducedvert[a]
+	if hozpositive > 1 and vertpositive > 1:
+		r = hozpositive * vertpositive
+	return r
+
+def matrixpooling(prevalence, matrixsize):
+	m = makematrix(prevalence, matrixsize)
+	h = reducepools(horizontal(m, matrixsize))
+	v = reducepools(vertical(m, matrixsize))
+	p = retestsamples(h, v, matrixsize)
+	"""
+	print(m)
+	print(h)
+	print(v)
+	printsamples(p)
+	"""
+	return matrixretests(h, v, matrixsize)
+
+def retestsamples(reducedhoz, reducedvert, matrixsize):
+	samples = []
+	hozpospools = []
+	vertpospools = []
+	unequivocal = False
+	if matrixretests(reducedhoz, reducedvert, matrixsize) == 0:
+		unequivocal = True
+	for a in range(matrixsize):
+		if reducedhoz[a] == 1:
+			hozpospools += [a]
+		if reducedvert[a] == 1:
+			vertpospools += [a]
+	for a in range(len(hozpospools)):
+		for b in range(len(vertpospools)):
+			samples += [(matrixsize * hozpospools[a]) + vertpospools[b]]
+	return [unequivocal, samples]
+
+def printsamples(rsamples):
+	if rsamples[0] == True:
+		poscount = len(rsamples[1])
+		if poscount > 1:
+			print("No retests, " + str(poscount) + " positive samples detected: " + str(rsamples[1]))
+		elif poscount == 1:
+			print("No retests, 1 positive sample detected: " + str(rsamples[1]))
+		else:
+			print("No retests, no positive samples detected.")
+	else:
+		print(str(len(rsamples[1])) + " retests: " + str(rsamples[1]))
 
 # this is not important right now
 def generatetray(trayx, trayy):
@@ -25,12 +105,28 @@ def generatetray(trayx, trayy):
 	return display
 
 def main():
-	i = 0
-	#for a in range(92):
-	for a in range(9200000):
-		i += traditional(0.05, 2)
+	i = [0, 0]
+	for a in range(12000):
+		for b in range(8):
+			i[0] += traditional(0.19, 2)
+		i[1] += matrixpooling(0.19, 4)
 	print(i)
-	print(i/100000)
+
+	i = [0, 0]
+	for a in range(12000):
+		for b in range(9):
+			i[0] += traditional(0.45, 2)
+		i[1] += matrixpooling(0.45, 3)
+		i[1] += matrixpooling(0.45, 3)
+	print(i)
+
+	# for a in range(1200000):
+	"""
+	i[0] *= 8 
+	i[0] /= 100000
+	i[1] /= 100000
+	print(i)
+	"""
 
 main()
 
